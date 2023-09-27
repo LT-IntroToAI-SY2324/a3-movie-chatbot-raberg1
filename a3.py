8f63a8676da2b26428977a21e33b883cf56d88f5
Robert Berg <raberg1@gmail.com>
2023-09-27 10:41:15 -0500

    finished title_by_year

--- a3.py
+++ a3.py
@@ -38,6 +38,10 @@ def get_year(movie: Tuple[str, str, int, List[str]]) -> int:
 def get_actors(movie: Tuple[str, str, int, List[str]]) -> List[str]:
     return movie[3]
 
+# print(get_title(movie_db[1]))
+# for movie in movie_db:
+#     print(get_title(movie))
+
 
 # Below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
@@ -54,7 +58,12 @@ def title_by_year(matches: List[str]) -> List[str]:
     Returns:
         a list of movie titles made in the passed in year
     """
-    pass
+    result = []
+    for movie in movie_db:
+        if int(matches[0]) == get_year(movie):
+            result.append(get_title(movie))
+    # print(result)
+    return result
 
 
 def title_by_year_range(matches: List[str]) -> List[str]:
@@ -226,7 +235,7 @@ def query_loop() -> None:
 
 if __name__ == "__main__":
     assert isinstance(title_by_year(["1974"]), list), "title_by_year not returning a list"
-    # assert isinstance(title_by_year_range(["1970", "1972"]), list), "title_by_year_range not returning a list"
+    assert isinstance(title_by_year_range(["1970", "1972"]), list), "title_by_year_range not returning a list"
     # assert isinstance(title_before_year(["1950"]), list), "title_before_year not returning a list"
     # assert isinstance(title_after_year(["1990"]), list), "title_after_year not returning a list"
     # assert isinstance(director_by_title(["jaws"]), list), "director_by_title not returning a list"
@@ -262,7 +271,7 @@ if __name__ == "__main__":
             "murray hamilton",
         ]
     ), "failed actors_by_title test"
-    assert sorted(actors_by_title(["movie not in database"])) == []), "failed actors_by_title not in database test"
+    assert sorted(actors_by_title(["movie not in database"])) == [], "failed actors_by_title not in database test"
     assert sorted(year_by_title(["jaws"])) == sorted(
         [1975]
     ), "failed year_by_title test"
